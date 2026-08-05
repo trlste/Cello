41170774708adf0585fed6ccc1f0b36e3fedb1a7
trlste <yumengren_999@outlook.com>
2026-08-05 18:14:45 -0400

    Initial Commit

---
+++ CellAnnotator/CellAnnotatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellAnnotatorApp.swift
+//  CellAnnotator
+//
+//  Created by Triss Ren on 2026/8/5.
+//
+
+import SwiftUI
+
+@main
+struct CellAnnotatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CellAnnotator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CellAnnotator
+//
+//  Created by Triss Ren on 2026/8/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
